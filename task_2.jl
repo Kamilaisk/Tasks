6284c9b44796253175967b70ac1301acacc5f914
Kamilaisk <72505703+Kamilaisk@users.noreply.github.com>
2020-12-24 12:41:54 +0300

    Update task_2.jl

--- task_2.jl
+++ task_2.jl
@@ -1,21 +1,23 @@
-function mark_frame_perimetr!(r::Robot)
+function mark_frame_perimetr!(r::Robot) #главная функция  
     num_vert = moves!(r, Sud)
     num_hor = moves!(r, Ost)
-    #Робот - в Юго-восточном углу
+    #Робот в Юго-Восточном углу
 
     for i in (0:3)
         side = HorizonSide(i)
         putmarkers!(r, side)
     end
-    #По всему периметру стоят маркеры
+    #По периметру стоят маркеры
 
     moves!(r, Nord, num_vert)
     moves!(r, West, num_hor)
-    #Робот - в исходном положении
+    #Робот в исходном положении
 end
 
 #вспомогательные функции
-function moves!(r::Robot, side::HorizonSide)
+
+#Перемещает робота в заданном направлении и считает шаги
+function moves!(r::Robot, side::HorizonSide) 
     num_steps=0
     while isborder(r,side)==false
         move!(r,side)
@@ -24,12 +26,14 @@ function moves!(r::Robot, side::HorizonSide)
     return num_steps
 end
 
+#Перемещает робота в заданном направлении по заданному количетсву шагов
 function moves!(r::Robot,side::HorizonSide,num_steps::Int)
     for _ in 1:numsteps #символ "_" заменяет неиспользуемую переменную
         move!(r,side)
     end
 end
 
+#Всюду в заданном направлении ставит маркеры вплоть до перегородки, но в исходной клетке маркер не ставит
 function putmarkers!(r::Robot, side::HorizonSide)
     while isborder(r,side)==false
         move!(r,side)
