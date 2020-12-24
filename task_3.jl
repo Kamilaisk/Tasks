01f4697f6409f1113920499f345d14e95cfa5ad1
Kamilaisk <72505703+Kamilaisk@users.noreply.github.com>
2020-12-24 12:40:14 +0300

    Update task_3.jl

--- task_3.jl
+++ task_3.jl
@@ -1,45 +1,35 @@
-function mark_all!(r::Robot) #Главная функция
+function mark_all!(r::Robot) #главная функция
 
     num_vert = moves!(r, Sud)
     num_hor = moves!(r, West)
-       #Робот-в Юго-Западном углу
+    #Робот в Юго-Западном углу
        
-       putmarker!(r)
-       putmarkers!(r,Ost)
-       #Робот в Юго-Восточном углу, вся нижняя линия промаркерована
+    putmarker!(r)
+    putmarkers!(r,Ost)
+    #Робот в Юго-Восточном углу, вся нижняя линия промаркерована
        
-       while isborder(r,Nord)==false
-           move!(r,Nord)
-           putmarker!(r)
-           if isborder(r,Ost)==false
-               putmarkers!(r,Ost)
-           else
-               putmarkers!(r,West)
-           end
-       end
-       #Робот поставил маркеры по всему периметру 
+    while isborder(r,Nord)==false
+        move!(r,Nord)
+        putmarker!(r)
+        if isborder(r,Ost)==false
+            putmarkers!(r,Ost)
+        else
+            putmarkers!(r,West)
+        end
+    end
+    #Робот поставил маркеры по всему полю 
      
-       moves!(r,West)
-       moves!(r,Sud)
-       moves!(r,Ost,num_hor)
-       moves!(r,Nord,num_vert)
+    moves!(r,West)
+    moves!(r,Sud)
+    moves!(r,Ost,num_hor)
+    moves!(r,Nord,num_vert)
+    #Робот - в исходном положении
    
 end
 
 #вспомогательные функции
-function moves!(r::Robot,side::HorizonSide,num_steps::Int) #Вспомогательная функция
-    for _ in 1:num_steps
-        move!(r,side)
-    end
-end
 
-function putmarkers!(r::Robot, side::HorizonSide)
-    while isborder(r,side)==false
-    move!(r,side)
-    putmarker!(r)
-    end
-end
-
+#Перемещает робота в заданном направлении и считает шаги
 function moves!(r::Robot, side::HorizonSide)
     num_steps=0
     while isborder(r,side)==false
@@ -48,3 +38,75 @@ function moves!(r::Robot, side::HorizonSide)
     end
     return num_steps
 end
+
+#Перемещает робота в заданном направлении по заданному количетсву шагов
+function moves!(r::Robot,side::HorizonSide,num_steps::Int) 
+    for _ in 1:num_steps
+        move!(r,side)
+    end
+end
+
+#Всюду в заданном направлении ставит маркеры вплоть до перегородки, но в исходной клетке маркер не ставит
+function putmarkers!(r::Robot, side::HorizonSide)
+    while isborder(r,side)==false
+    move!(r,side)
+    putmarker!(r)
+    end
+end
+
+function mark_all!(r::Robot) #главная функция
+
+    num_vert = moves!(r, Sud)
+    num_hor = moves!(r, West)
+    #Робот в Юго-Западном углу
+       
+    putmarker!(r)
+    putmarkers!(r,Ost)
+    #Робот в Юго-Восточном углу, вся нижняя линия промаркерована
+       
+    while isborder(r,Nord)==false
+        move!(r,Nord)
+        putmarker!(r)
+        if isborder(r,Ost)==false
+            putmarkers!(r,Ost)
+        else
+            putmarkers!(r,West)
+        end
+    end
+    #Робот поставил маркеры по всему полю 
+     
+    moves!(r,West)
+    moves!(r,Sud)
+    moves!(r,Ost,num_hor)
+    moves!(r,Nord,num_vert)
+    #Робот - в исходном положении
+   
+end
+
+#вспомогательные функции
+
+#Перемещает робота в заданном направлении и считает шаги
+function moves!(r::Robot, side::HorizonSide)
+    num_steps=0
+    while isborder(r,side)==false
+         move!(r,side)
+         num_steps+=1
+    end
+    return num_steps
+end
+
+#Перемещает робота в заданном направлении по заданному количетсву шагов
+function moves!(r::Robot,side::HorizonSide,num_steps::Int) 
+    for _ in 1:num_steps
+        move!(r,side)
+    end
+end
+
+#Всюду в заданном направлении ставит маркеры вплоть до перегородки, но в исходной клетке маркер не ставит
+function putmarkers!(r::Robot, side::HorizonSide)
+    while isborder(r,side)==false
+    move!(r,side)
+    putmarker!(r)
+    end
+end
+
