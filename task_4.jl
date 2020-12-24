c866df699a44dd9f9c75d33c94eee65ccad3a212
Kamilaisk <72505703+Kamilaisk@users.noreply.github.com>
2020-12-24 12:42:56 +0300

    Update task_4.jl

--- task_4.jl
+++ task_4.jl
@@ -1,11 +1,11 @@
-function mark_lestnica!(r::Robot)
+function mark_lestnica!(r::Robot) #главная функция
     num_vert = moves!(r, Sud)
     num_hor = moves!(r, West)
-       #обот в Юго-Западном углу
+    #Робот в Юго-Западном углу
        
-       putmarker!(r)
-       k = putmarkers_0!(r,Ost)
-       #Робот в Юго-Восточном углу, вся нижняя линия промаркерована
+    putmarker!(r)
+    k = putmarkers_0!(r,Ost)
+    #Робот в Юго-Восточном углу, вся нижняя линия промаркерована
 
     for i in 1:k
         if isborder(r,Nord) == false
@@ -21,44 +21,44 @@ function mark_lestnica!(r::Robot)
             end
         end
     end
+    #Робот сделал лестницу
 
     moves!(r,West)
     moves!(r,Sud)
     moves!(r,Ost,num_hor)
     moves!(r,Nord,num_vert)
+    #Робот - в исходном положении
 
 end
 
 #вспомогательные функции
-function moves!(r::Robot,side::HorizonSide,num_steps::Int) #Вспомогательная функция
-    for _ in 1:num_steps
-        move!(r,side)
-    end
-end
 
-function putmarkers!(r::Robot, side::HorizonSide)
- while isborder(r,side)==false
-     move!(r,side)
-     putmarker!(r)
- end
-end
-
- function moves!(r::Robot, side::HorizonSide)
-     num_steps=0
-     while isborder(r,side)==false
+#Перемещает робота в заданном направлении и считает шаги
+function moves!(r::Robot, side::HorizonSide)
+    num_steps=0
+    while isborder(r,side)==false
          move!(r,side)
          num_steps+=1
-     end
-     return num_steps
- end
+    end
+    return num_steps
+end
 
- function putmarkers_2!(r::Robot, side::HorizonSide, num_steps::Int)
+#Перемещает робота в заданном направлении по заданному количетсву шагов
+function moves!(r::Robot,side::HorizonSide,num_steps::Int) 
     for _ in 1:num_steps
         move!(r,side)
-        putmarker!(r)
     end
 end
 
+#Всюду в заданном направлении ставит маркеры вплоть до перегородки, но в исходной клетке маркер не ставит
+function putmarkers!(r::Robot, side::HorizonSide)
+    while isborder(r,side)==false
+    move!(r,side)
+    putmarker!(r)
+    end
+end
+
+#Всюду в заданном направлении ставит маркеры вплоть до перегородки, но в исходной клетке маркер не ставит и считает количество сделанных шагов
 function putmarkers_0!(r::Robot,side::HorizonSide)
     num_k=0
      while isborder(r,side)==false
@@ -69,4 +69,10 @@ function putmarkers_0!(r::Robot,side::HorizonSide)
      return num_k
  end
 
-                
+ #Возвращает изначально сделанные шаги, но также ставит маркеры
+ function putmarkers_2!(r::Robot, side::HorizonSide, num_steps::Int)
+    for _ in 1:num_steps
+        move!(r,side)
+        putmarker!(r)
+    end
+end
