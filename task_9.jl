e8372a8cc6f3cae571fd278851e4b18d08642a05
Kamilaisk <72505703+Kamilaisk@users.noreply.github.com>
2020-12-25 15:35:05 +0300

    Задача 9

---
+++ task_9.jl
@@ -0,0 +1,23 @@
+function find_marker(r) #ищем маркер на поле
+    num_steps_max=1
+    side=Nord
+    while ismarker(r)==false
+        for _ in 1:2
+            find_marker(r,side,num_steps_max)
+            side=next(side)
+        end
+        num_steps_max+=1
+    end
+end
+
+#вспомогательные фукнции
+function find_marker(r,side,num_steps_max)
+    for _ in 1:num_steps_max
+        if ismarker(r)
+            return nothing
+        end
+        move!(r,side)
+    end
+end
+
+next(side::HorizonSide)=HorizonSide(mod(Int(side)+1,4))
