a98d90c6e67d6ee81f31ffb8ef37b394d2d2dfbc
Kamilaisk <72505703+Kamilaisk@users.noreply.github.com>
2020-12-26 09:30:25 +0300

    Update task_16.jl

--- task_16.jl
+++ task_16.jl
@@ -11,6 +11,32 @@ function mark_field(r) #главная функция
 end
 
 #вспомогательные функции
+function move_if_possible!(r::Robot, direct_side::HorizonSide)::Bool
+    orthogonal_side = next(direct_side)
+    reverse_side = reverse(orthogonal_side)
+    num_steps=0
+    while isborder(direct_side) == true
+        if isborder(r, orthogonal_side) == false
+            move(r, orthogonal_side)
+            num_steps += 1
+        else
+            break
+        end
+    end
+    # Робот или уперся в угол внешней рамки поля, или готов сделать шаг (или несколько) в направлении 
+    # direct_side
+    if isborder(r,direct_side) == false
+        while isborder(r,reverse_side) == true
+            move!(r,direct_side)
+        end
+        result = true
+    else
+        result = false
+    end
+    move!(r,reverse_side)
+    return result
+end
+
 function mark_row(r::Robot,side::HorizonSide)
     putmarker!(r)
     while move_if_posible!(r,side) == true
@@ -18,7 +44,35 @@ function mark_row(r::Robot,side::HorizonSide)
     end
 end
 
-movements!(r::Robot, side::HorizonSide, num_steps::Int) =
-    for _ in 1:num_steps
-        move_if_posible!(r,side) # обход внутренней перегородки гарантирована
+function move_if_possible!(r::Robot, direct_side::HorizonSide)::Bool
+    orthogonal_side = next(direct_side)
+    reverse_side = reverse(orthogonal_side)
+    num_steps=0
+    while isborder(direct_side) == true
+        if isborder(r, orthogonal_side) == false
+            move(r, orthogonal_side)
+            num_steps += 1
+        else
+            break
+        end
     end
+    #Робот или уперся в угол внешней рамки поля, или готов сделать шаг (или несколько) в направлении 
+    # direct_side
+    if isborder(r,direct_side) == false
+        while isborder(r,reverse_side) == true
+            move!(r,direct_side)
+        end
+        result = true
+    else
+        result = false
+    end
+    move!(r,reverse_side)
+    return result
+end
+
+movements!(r::Robot, side::HorizonSide, num_steps::Int) =
+for _ in 1:num_steps
+    move_if_posible!(r,side) # - в данном случае возможность обхода внутренней перегородки гарантирована
+end
+
+    
